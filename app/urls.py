62631b216d77b5559e5e97352e5622fc2b19aacc
StudentBI3 <StudentBI3@SYN13ASS-NB-508.alabuga.local>
2024-12-27 15:22:58 +0300

    add upate

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='main-page'),
     path('create_product', views.ProductCreateView.as_view(), name='create-product'),
+    path('update_product/<int:pk>', views.ProductUpdateView.as_view(), name='update-product'),
 ]
