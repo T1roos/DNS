7da3e96395a4e478d633a0bd78c2a3c3d8323e97
StudentBI3 <StudentBI3@SYN13ASS-NB-508.alabuga.local>
2024-12-27 14:51:19 +0300

    add create

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='main-page'),
+    path('create_product', views.ProductCreateView.as_view(), name='create-product'),
 ]
